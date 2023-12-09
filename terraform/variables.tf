4cde6266059fff0b69510999b5fbeb061b0528fb
Paulo Cardoso <ph_cardoso@hotmail.com>
2023-12-09 18:30:20 -0300

    first commit

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,10 +2,9 @@ variable "region" {
   description = "AWS region"
   type        = string
   default     = "us-east-2"
-}
 
 variable "clusterName" {
   description = "Name of the EKS cluster"
   type        = string
-  default     = "kitops-eks"
+  default     = "vprofile-eks"
 }
